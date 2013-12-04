8bf2dbd8993d17361b3e772612900cee1368fe7c
tristaf <tristaf@opeanuts.org>
2013-12-04 17:20:32 +0100

    Update build script: use clang compiler and automatically retrieve MacsOSX SDK version

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-export PATH=/Applications/Xcode.app/Contents/Developer/usr/bin
-
-gcc -o fruitstrap -framework CoreFoundation -framework MobileDevice -F/System/Library/PrivateFrameworks fruitstrap.c -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.6.sdk
+DEV_PATH=`xcode-select -print-path`
+SDKVERSION=`ls ${DEV_PATH}/Platforms/MacOSX.platform/Developer/SDKs | awk 'match($0,/MacOSX.*sdk/){print substr($0,RSTART+6,4)}'`
+CC=${DEV_PATH}/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang
+${CC} -o fruitstrap -framework CoreFoundation -framework MobileDevice -F/System/Library/PrivateFrameworks fruitstrap.c -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX${SDKVERSION}.sdk
